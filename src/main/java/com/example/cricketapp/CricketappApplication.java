58318b6eb8fdcf569462d8a4018d623a1069a9d1
Akshitha Krishna <akshithacreativecreation@gmail.com>
2026-05-02 23:50:12 +0530

    Initial commit - Cricket Spring Boot project

---
+++ src/main/java/com/example/cricketapp/CricketappApplication.java
@@ -0,0 +1,13 @@
+package com.example.cricketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketappApplication.class, args);
+	}
+
+}
